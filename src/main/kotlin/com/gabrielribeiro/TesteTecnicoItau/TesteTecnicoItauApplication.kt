ce4126a61b537691e29f10300f63fc270a75179f
gribeiro5000 <gribeiro.gr17@gmail.com>
2025-01-27 06:51:29 -0300

    first commit

---
+++ src/main/kotlin/com/gabrielribeiro/TesteTecnicoItau/TesteTecnicoItauApplication.kt
@@ -0,0 +1,11 @@
+package com.gabrielribeiro.TesteTecnicoItau
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TesteTecnicoItauApplication
+
+fun main(args: Array<String>) {
+	runApplication<TesteTecnicoItauApplication>(*args)
+}
---
+++ src/test/kotlin/com/gabrielribeiro/TesteTecnicoItau/TesteTecnicoItauApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabrielribeiro.TesteTecnicoItau
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TesteTecnicoItauApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
